96b1f1824f57eda02baa2a8cf5e1e5251f7d296b
xtsoulfiretx <alexi.lambrou@gmail.com>
2021-10-25 17:27:05 +0100

    Removed unnecessary rules/ styling

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,42 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="nav-container">
+    <router-link v-bind:to="'/'">Home</router-link>
+    <router-link v-bind:to="'/directory'">Business Directory</router-link>
+  </div>
+  <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
 }
 </script>
-
-<style lang="scss">
+<!-- styling for the component -->
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-
-h1, h2 {
-  font-weight: normal;
+body {
+  margin: 0;
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
 a {
-  color: #42b983;
+  color: #fff;
+  padding: 10px;
+
+}
+.nav-container {
+    height: 2rem;
+    background-color: #000;
+    color: #fff;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    font-family: dco-heading-font,sans-serif;
+    text-transform: uppercase;
+    font-weight: 600;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,42 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+//import the vue instance
 import Vue from 'vue'
-import App from './App.vue'
-import Router from 'vue-router'
+//import the App component
+import App from './App'
+//import the vue router
+import VueRouter from 'vue-router'
+//Import Axios
+import axios from 'axios'
+//import the Homepage component
+import Homepage from './components/Homepage'
+//import the Directory component
+import Directory from './components/Directory'
+//tell vue to use the router
+Vue.use(VueRouter)
+//define your routes
+const routes = [
+//define the root url of the application.
+{ path: '/', component: Homepage },
+{ path: '/directory', component: Directory}
+]
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+
+// Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes, // short for routes: routes
+  mode: 'history'
 })
+//instatinat the vue instance
+new Vue({
+//define the selector for the root component
+  el: '#app',
+  //pass the template to the root component
+  template: '<App/>',
+  //declare components that the root component can access
+  components: { App },
+  //pass in the router to the Vue instance
+  router
+}).$mount('#app')//mount the router on the app
